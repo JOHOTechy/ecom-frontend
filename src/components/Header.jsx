15b160da3387b2deaf5d69334bacdd3c581c6e1c
JOHOTechy <johotechy@gmail.com>
2026-06-11 23:24:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ export default function Header({ onSearch }) {
           <a href="/" className="header__logo" id="logo-link">
             <span className="header__logo-icon">⚡</span>
             <span className="header__logo-text">
-              zet<span className="header__logo-accent">op Sandeep Learning</span>
+              zet<span className="header__logo-accent">op Jenkins</span>
             </span>
           </a>
 
